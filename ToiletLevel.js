2ed554a03ccd06941495a6655b1260283e6fbfd2
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2024-02-06 21:54:08 -0500

    Created ToiletLevel.html, and linked it to ToiletLevel.js, which is also revised
    The canvas is showing in the function myGameArea

--- ToiletLevel.js
+++ ToiletLevel.js
@@ -1,13 +1,24 @@
+// Zesh: Ensures the startGame function is called once the document is fully loaded
+// Zesh: This is an event listener
+document.addEventListener('DOMContentLoaded', startGame);
+
 function startGame() {
     myGameArea.start();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
+    canvas: document.createElement("canvas"),
+    start: function() {
         this.canvas.width = 1920;
         this.canvas.height = 1080;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.fillBackground();
+    },
+
+    fillBackground: function() {
+        // Set a background color
+        this.context.fillStyle = "#76B947"; // Example: a light green color
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
