24a39253efb59ed2c01474142e7fcb1864ddcff7
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2024-02-06 22:15:41 -0500

    added resizeCanvas() function
    now the canvas is scalable according to windows size, but we may want to revise it when dealing with images and objects appearing there as well

--- ToiletLevel.js
+++ ToiletLevel.js
@@ -1,24 +1,34 @@
 // Zesh: Ensures the startGame function is called once the document is fully loaded
-// Zesh: This is an event listener
+// Zesh: This is an event listener. It's powerful and you can implement many things using it
 document.addEventListener('DOMContentLoaded', startGame);
 
+// Main game function
 function startGame() {
     myGameArea.start();
+    resizeCanvas();
 }
 
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function() {
-        this.canvas.width = 1920;
-        this.canvas.height = 1080;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.fillBackground();
     },
 
     fillBackground: function() {
-        // Set a background color
-        this.context.fillStyle = "#76B947"; // Example: a light green color
+        this.context.fillStyle = "#76B947"; // a light green color, just for testing canvas is showing
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
+
+// Adjusts canvas size according to windows size
+// REMINDER: changing the canvas size can affect the
+// positioning and scaling of our drawn elements,
+// so this function needs to be adjusted in the future
+function resizeCanvas() {
+    myGameArea.canvas.width = window.innerWidth;
+    myGameArea.canvas.height = window.innerHeight;
+
+    myGameArea.fillBackground(); // refill background after resizing
+}
+
